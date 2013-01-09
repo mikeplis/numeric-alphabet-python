3e3fef477636384509b0541388bb408c4919c897
Michael Plis <michael.plis01@gmail.com>
2013-01-09 10:27:04 -0500

    WIP; updated mappings; thinking dictionary words should be sorted with number translation to make substring finding easier

--- translator.py
+++ translator.py
@@ -1,24 +1,30 @@
 mapping = {
-    "0": ["S", "Z"],
+    "0": ["S", "Z", "ZH"],
     "1": ["T", "D"],
     "2": ["N"],
     "3": ["M"],
     "4": ["R", "ER"],
     "5": ["L"],
-    "6": ["SH", "JH", "CH", "TH"],
+    "6": ["SH", "JH", "CH", "TH", "DH"],
     "7": ["G", "K"],
     "8": ["F", "V"],
     "9": ["B", "P"]
     }
 
-# vowels = ["AH", "IH", "IY", "EH", "AA", "AE", "EY", "AO", "AY", "AW", "OY", "UH"]
+# vowels = ["AH", "IH", "IY", "EH", "AA", "AE", "EY", "AO", "AY", "AW", "OY", "UH", "Y"]
+# unused = ["W", "HH", "NG"]
 dict_ = {}
 
-def findIndices(root_number, dict_word):
+def find_indices(root_number, dict_word):
     """ find starting and ending index of dict word phonemes in root number """
+    for index, phoneme in enumerate(dict_[dict_word]):        
+        for key in mapping:
+            # if phoneme in mapping[key] and str(root_number[index]) ==
+            pass
+        
+        
+def phonemes_to_numstring():
     pass
-        
-        
 
 class WordNode(object):
     def __init__(self, word, start_index, end_index):
@@ -39,11 +45,14 @@ while True:
     words = []
     try:
         input_string = input("Number to translate: ")
+        if len(input_string) == 0:
+            print("You didn't enter anything, try again.")
+            continue
         for number in input_string:
             root_mapping.append(mapping[number])
     except KeyError:
         print("That string contained a character that's not a number. Try again: ")
-
+    
     for key in dict_:
         if len(dict_[key]) == len(root_mapping):            
             for index, t in enumerate(root_mapping):
